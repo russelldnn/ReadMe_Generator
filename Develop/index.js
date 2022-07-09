12fd5a8f93062d05df9594d2e626e5627b704a24
russelldnn <russelldnn@gmail.com>
2022-07-09 11:38:55 -0500

    added relevant questions to inquire about later

--- Develop/index.js
+++ Develop/index.js
@@ -3,7 +3,69 @@
     let fs = require ('fs');
     let inquirer = require ('inquirer');
 // TODO: Create an array of questions for user input
-const questions = [];
+const questions = [
+    {
+        type: 'input',
+        name: 'title',
+        message: 'What is the name of your application?'
+    },
+
+    {
+        type: 'input',
+        name: 'repoName',
+        message: 'What is the name of your applications github repo?'
+    },
+
+    {
+        type: 'input',
+        name: 'description',
+        message: 'Add a description for your application'
+    },
+
+    {
+        type: 'input',
+        name: 'install',
+        message: 'How do you install the app?'
+    },
+
+    {
+        type: 'input',
+        name: 'goal',
+        message: 'how is this app meant to be used?'
+    },
+
+    {
+        type: 'input',
+        name: 'acknowledgements',
+        message: 'List all contributors for this project'
+    },
+
+    {
+        type: 'input',
+        name: 'tests',
+        message: 'What tests were used in this application'
+    },
+
+    {
+        type: 'list',
+        name: 'lisense',
+        message: 'what license(s) are applied to this app?',
+        choices: ["AAL", "ISC", "MIT", "NTP", "W3C"]
+    },
+
+    {
+        type: 'input',
+        name: 'github',
+        message: 'what is your github username?'
+    },
+
+    {
+        type: 'input',
+        name: 'email',
+        message: 'what is your email address?'
+    }
+
+];
 
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {}
